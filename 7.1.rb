3b9f5dda267eac5f9ede619f923d394c3f05c3bb
Yanming Deng <cisolarix@gmail.com>
2012-08-19 09:31:13 +0800

    preceding 7 chapters finished, on chapter 8

--- 7.1.rb
+++ 7.1.rb
@@ -1,3 +1,10 @@
-p "10, 20, 30, 40".split(",")
-p [1, 2, 3, 8].index(8)
-p 100.integer?
+# p "10, 20, 30, 40".split(",")
+# p [1, 2, 3, 8].index(8)
+# p 100.integer?
+
+def hello(name)
+    print "hello, ", name, "\n"
+    
+end
+
+hello('cisolarix')
---
+++ 7.2.rb
@@ -0,0 +1,6 @@
+def hello(name = "Ruby")
+    print "hello, ", name, "\n"
+    
+end
+hello()
+hello('newbie')
